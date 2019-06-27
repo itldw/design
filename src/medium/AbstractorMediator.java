098eb80eb9044fd3e4687c36b433bbe7caf1b4c8
lidawei <lidawei@iciyun.com>
2019-06-27 17:34:53 +0800

    中介者

--- src/medium/AbstractorMediator.java
+++ src/medium/AbstractorMediator.java
@@ -10,8 +10,12 @@ package medium;
 public abstract class AbstractorMediator {
 
     protected ContreateColleague c1;
-    protected ContreateColleague c2;
+    protected ContreateColleague2 c2;
 
+    public  AbstractorMediator(){
+        c1=new ContreateColleague(this);
+        c2=new ContreateColleague2(this);
+    }
     public abstract void doSomething1();
     public abstract void doSomething2();
 
@@ -23,11 +27,12 @@ public abstract class AbstractorMediator {
         this.c1 = c1;
     }
 
-    public ContreateColleague getC2() {
+    public ContreateColleague2 getC2() {
         return c2;
     }
 
-    public void setC2(ContreateColleague c2) {
+    public void setC2(ContreateColleague2 c2) {
         this.c2 = c2;
     }
+
 }
